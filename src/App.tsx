5313aa007010a4a06bfae344cc0f5bbfdbf303da
unknown <nanibonbino22@gmail.com>
2024-05-17 19:09:48 -0400

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
   return (
     <main>
       <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
+      <button onClick={createTodo}>+ test new</button>
       <ul>
         {todos.map((todo) => (
           <li key={todo.id}>{todo.content}</li>

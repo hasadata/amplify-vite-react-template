01e9a9a127cbe192a721804f96304e34a6655936
unknown <nanibonbino22@gmail.com>
2024-05-18 18:55:34 -0400

    test

---
+++ src/components/RandomCookieGenerator.tsx
@@ -0,0 +1,21 @@
+import React, { useEffect } from 'react';
+
+const generateRandomCookie = () => {
+    const randomValue = Math.random().toString(36).substring(2, 15);
+    document.cookie = `randomCookie=${randomValue}; path=/`;
+};
+
+const RandomCookieGenerator = () => {
+    useEffect(() => {
+        generateRandomCookie();
+    }, []);
+
+    return (
+        <div>
+            <h1>Random Cookie Generator</h1>
+            <p>A new random cookie is set on each request.</p>
+        </div>
+    );
+};
+
+export default RandomCookieGenerator;
